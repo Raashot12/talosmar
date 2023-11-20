26912a4cfcb8d3bb5de8e896fb7744dcce4e075a
Rasheed Iskilu <riskilu@plateaumed.com>
2023-11-20 19:18:00 +0100

    done

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,16 +1,48 @@
+"use client"
 import InputBox from "@/components/CreatePost"
 import Header from "@/components/Header"
-import PostCard from "@/components/PostCard"
+import PostCard from "./PostCard"
+import axios from "axios"
+import Cookies from "js-cookie"
+import {toast} from "react-toastify"
+import {useEffect, useState} from "react"
 
+export interface Post {
+  username: string
+  base64str: string | null
+  post: string
+  created_at: string
+}
 export default function Home() {
+  const [data, setData] = useState<Post[]>([])
+  const fetchPost = async () => {
+    const email = Cookies.get("loggedin")
+    try {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_API}/api/posts/${email}`
+      )
+      const responseData = await response
+      if (responseData.status === 200) {
+        setData(responseData.data)
+      }
+    } catch (error) {
+      toast.error("An unexpected error occurred", {
+        position: toast.POSITION.TOP_LEFT,
+      })
+    }
+  }
+  useEffect(() => {
+    fetchPost()
+  }, [])
+  console.log(data)
   return (
     <main>
       <Header />
       <div className="w-full xl:w-1/2 mx-auto">
         <InputBox />
       </div>
-      <div className="w-full xl:w-1/2 mx-auto">
-        <PostCard />
+      <div className="w-full xl:w-1/2 mx-auto mt-9">
+        <PostCard post={data as Post[]} />
       </div>
     </main>
   )
---
+++ src/app/(root)/postCard.tsx
@@ -0,0 +1,70 @@
+"use client"
+
+import React from "react"
+import styled from "styled-components"
+import {Post} from "./page"
+
+function PostCard({post}: {post: Post[]}) {
+  return (
+    <div
+      style={{fontFamily: "Inter"}}
+      className="mb-7 bg-white flex flex-col justify-start rounded-2xl shadow-md"
+    >
+      {post.length === 0 ? (
+        <div className="align-middle">No Feed to show</div>
+      ) : (
+        post.map((value, index) => {
+          return (
+            <div className="p-4" key={index}>
+              <div className="flex space-x-3 items-center ml-2 relative">
+                <Image src={value.base64str as string} alt="userimg" />
+                <div>
+                  <p
+                    style={{
+                      fontSize: ".91rem",
+                    }}
+                    className="text-gray-500 font-light"
+                  >
+                    {value.created_at}
+                  </p>
+                </div>
+              </div>
+              <p className="ml-2 mt-5">{value.post}</p>
+            </div>
+          )
+        })
+      )}
+    </div>
+  )
+}
+
+export default PostCard
+
+const Image = styled.img`
+  object-fit: cover;
+  height: 2.95rem;
+  width: 2.95rem;
+  border-radius: 50%;
+`
+
+const PostImage = styled.img`
+  object-fit: contain;
+  height: auto;
+  max-height: 455px;
+  width: 100%;
+  margin-top: 0.35rem;
+  margin-bottom: 1.2rem;
+  transition: all 0.22s ease-out;
+  border-top: 0.7px solid lightgrey;
+  border-bottom: 0.7px solid lightgrey;
+`
+
+const UserPTag = styled.p`
+  cursor: pointer;
+  margin-bottom: -0.09rem;
+  font-weight: 500;
+  font-size: 1.05rem;
+  :hover {
+    text-decoration: underline;
+  }
+`
